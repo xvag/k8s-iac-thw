8a5ebf7ff462f73d66c55fc4a483b4d07f42c282
xvag <xvag@protonmail.com>
2022-06-22 13:41:56 +0300

    move in-cluster fw to separate file

--- gcp/controller.tf
+++ gcp/controller.tf
@@ -12,26 +12,8 @@ resource "google_compute_subnetwork" "controller-subnet" {
   network       = google_compute_network.controller-vpc.id
 }
 
-resource "google_compute_firewall" "controller-fw-in" {
-  name     = "controller-fw-in"
-  network  = "controller-vpc"
-  allow {
-    protocol = "tcp"
-  }
-  allow {
-    protocol = "udp"
-  }
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["${var.vpc.controller.subnet}","${var.vpc.worker.subnet}","${var.pod-cidr-range}"]
-  depends_on = [
-    google_compute_subnetwork.controller-subnet
-  ]
-}
-
-resource "google_compute_firewall" "controller-fw-ex" {
-  name     = "controller-fw-ex"
+resource "google_compute_firewall" "controller-fw" {
+  name     = "controller-fw"
   network  = "controller-vpc"
   allow {
     protocol = "tcp"
--- gcp/worker.tf
+++ gcp/worker.tf
@@ -12,26 +12,8 @@ resource "google_compute_subnetwork" "worker-subnet" {
   network       = google_compute_network.worker-vpc.id
 }
 
-resource "google_compute_firewall" "worker-fw-in" {
-  name     = "worker-fw-in"
-  network  = "worker-vpc"
-  allow {
-    protocol = "tcp"
-  }
-  allow {
-    protocol = "udp"
-  }
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["${var.vpc.controller.subnet}","${var.vpc.worker.subnet}","${var.pod-cidr-range}"]
-  depends_on = [
-    google_compute_subnetwork.worker-subnet
-  ]
-}
-
-resource "google_compute_firewall" "worker-fw-ex" {
-  name     = "worker-fw-ex"
+resource "google_compute_firewall" "worker-fw" {
+  name     = "worker-fw"
   network  = "worker-vpc"
   allow {
     protocol = "tcp"
