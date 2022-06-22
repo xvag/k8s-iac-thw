ee6916f88c0ea1c3a8f49adeee40eec8835f326f
xvag <xvag@protonmail.com>
2022-06-22 18:06:48 +0300

    fix try

--- gcp/project.tf
+++ gcp/project.tf
@@ -10,3 +10,9 @@ provider "google" {
   project     = var.gcp_project
   credentials = var.gcp_creds
 }
+
+resource "google_compute_project_metadata_item" "ssh-keys" {
+  project = var.gcp_project
+  key     = "ssh-keys"
+  value   = "${var.ssh_user}:${var.ssh_key}"
+}
