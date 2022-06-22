eb2e15822a3362d913e50c523e1bb011d4f3113a
xvag <xvag@protonmail.com>
2022-06-22 23:49:47 +0300

    fix fw

--- gcp/firewalls.tf
+++ gcp/firewalls.tf
@@ -17,20 +17,29 @@ module "fw-in-cluster-w" {
   src_ranges = ["${var.vpc.controller.cidr}","${var.vpc.worker.cidr}","${var.pod_cidr_range}"]
 }
 
-module "c-fw-ex" {
+module "fw_allow_health_checks" {
   source     = "./modules/firewall"
-  fw_name    = "c-fw-ex"
+  fw_name    = "fw_allow_health_checks"
   vpc_name   = module.c-vpc.vpc_name
   vpc_subnet = module.c-vpc.subnet_name
-  rules      = var.fw_ex_c
+  rules      = var.fw_allow_health_checks
+  src_ranges = ["209.85.152.0/22", "209.85.204.0/22", "35.191.0.0/16"]
+}
+
+module "fw_controller_vpc" {
+  source     = "./modules/firewall"
+  fw_name    = "fw_controller_vpc"
+  vpc_name   = module.c-vpc.vpc_name
+  vpc_subnet = module.c-vpc.subnet_name
+  rules      = var.fw_controller_vpc
   src_ranges = ["0.0.0.0/0"]
 }
 
-module "w-fw-ex" {
+module "fw_worker_vpc" {
   source     = "./modules/firewall"
-  fw_name    = "w-fw-ex"
+  fw_name    = "fw_worker_vpc"
   vpc_name   = module.w-vpc.vpc_name
   vpc_subnet = module.w-vpc.subnet_name
-  rules      = var.fw_ex_w
+  rules      = var.fw_worker_vpc
   src_ranges = ["0.0.0.0/0"]
 }
--- gcp/healthchecks.tf
+++ gcp/healthchecks.tf
@@ -7,15 +7,6 @@ module "k8s-ip" {
   vpc_region   = var.vpc.controller.region
 }
 
-module "fw-ahc" {
-  source     = "./modules/firewall"
-  fw_name    = "fw-ahc"
-  vpc_name   = module.c-vpc.vpc_name
-  vpc_subnet = module.c-vpc.subnet_name
-  rules      = var.fw_ahc
-  src_ranges = ["209.85.152.0/22", "209.85.204.0/22", "35.191.0.0/16"]
-}
-
 resource "google_compute_http_health_check" "k8s-health-check" {
   name         = "k8s-health-check"
   host         = "kubernetes.default.svc.cluster.local"
