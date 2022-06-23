5cfdacfb36ca4ae19ef7cfaa4a102b975e6d8198
xvag <xvag@protonmail.com>
2022-06-23 12:24:10 +0300

    move firewalls to networks manifest

--- gcp/network.tf
+++ gcp/network.tf
@@ -1,3 +1,6 @@
+###
+### VPC/Subnets
+###
 
 module "c-vpc" {
   source        = "./modules/vpc"
@@ -15,6 +18,10 @@ module "w-vpc" {
   subnet_cidr   = var.vpc.worker.cidr
 }
 
+###
+### VPC Peerings
+###
+
 module "c-peer" {
   source    = "./modules/peer"
   peer_name = "c-w"
@@ -28,6 +35,59 @@ module "w-peer" {
   net       = module.w-vpc.vpc_self_link
   net_peer  = module.c-vpc.vpc_self_link
 }
+
+###
+### Firewalls
+###
+
+module "fw-in-cluster-c" {
+  source     = "./modules/firewall"
+  fw_name    = "fw-in-cluster-c"
+  vpc_name   = module.c-vpc.vpc_name
+  vpc_subnet = module.c-vpc.subnet_name
+  rules      = var.fw.in_cluster
+  src_ranges = ["${var.vpc.controller.cidr}","${var.vpc.worker.cidr}","${var.pod_cidr_range}"]
+}
+
+module "fw-in-cluster-w" {
+  source     = "./modules/firewall"
+  fw_name    = "fw-in-cluster-w"
+  vpc_name   = module.w-vpc.vpc_name
+  vpc_subnet = module.w-vpc.subnet_name
+  rules      = var.fw.in_cluster
+  src_ranges = ["${var.vpc.controller.cidr}","${var.vpc.worker.cidr}","${var.pod_cidr_range}"]
+}
+
+module "fw-allow-health-checks" {
+  source     = "./modules/firewall"
+  fw_name    = "fw-allow-health-checks"
+  vpc_name   = module.c-vpc.vpc_name
+  vpc_subnet = module.c-vpc.subnet_name
+  rules      = var.fw.allow_health_checks
+  src_ranges = ["209.85.152.0/22", "209.85.204.0/22", "35.191.0.0/16"]
+}
+
+module "fw-controller-vpc" {
+  source     = "./modules/firewall"
+  fw_name    = "fw-controller-vpc"
+  vpc_name   = module.c-vpc.vpc_name
+  vpc_subnet = module.c-vpc.subnet_name
+  rules      = var.fw.controller_vpc
+  src_ranges = ["0.0.0.0/0"]
+}
+
+module "fw-worker-vpc" {
+  source     = "./modules/firewall"
+  fw_name    = "fw-worker-vpc"
+  vpc_name   = module.w-vpc.vpc_name
+  vpc_subnet = module.w-vpc.subnet_name
+  rules      = var.fw.worker_vpc
+  src_ranges = ["0.0.0.0/0"]
+}
+
+###
+### Routes
+###
 
 module "k8s-pods-route" {
   count = 3
